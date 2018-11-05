4ddfaf5f78d19d9fd8abf900211062a6dbbbecd9
Abishek Venkit <abishekvenkit@gmail.com>
2018-11-05 00:44:39 -0600

    d

--- app.py
+++ app.py
@@ -46,5 +46,4 @@ def category_passholder():
 	return render_template("category_passholder.html")
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT',5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
