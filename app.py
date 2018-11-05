a96a10a8e272319b54314d7fbdc388bec49b04dd
Abishek Venkit <abishekvenkit@gmail.com>
2018-11-05 01:33:36 -0600

    Final Files Uploaded

--- app.py
+++ app.py
@@ -21,29 +21,29 @@ Bootstrap(app)
 def home_page():
 	return render_template("home.html")
 
-# @app.route("/data_visuals")
-# def data_visuals():
-# 	return render_template("data_visuals.html")
+@app.route("/data_visuals")
+def data_visuals():
+	return render_template("data_visuals.html")
 
-# @app.route("/location_analysis")
-# def location_analysis():
-# 	return render_template("popular_locs_map.html")
+@app.route("/location_analysis")
+def location_analysis():
+	return render_template("popular_locs_map.html")
 
-# @app.route("/distance_riders")
-# def distance_riders():
-# 	return render_template("distance_riders.html")
+@app.route("/distance_riders")
+def distance_riders():
+	return render_template("distance_riders.html")
 
 @app.route("/seasons")
 def seasons():
 	return render_template("seasons.html")
 
-# @app.route("/bike_logistics")
-# def bike_logistics():
-# 	return render_template("bike_logistics.html")
+@app.route("/bike_logistics")
+def bike_logistics():
+	return render_template("bike_logistics.html")
 
-# @app.route("/category_passholder")
-# def category_passholder():
-# 	return render_template("category_passholder.html")
+@app.route("/category_passholder")
+def category_passholder():
+	return render_template("category_passholder.html")
 
 if __name__ == "__main__":
     app.run()
